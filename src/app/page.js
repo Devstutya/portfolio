8e604138ef4281cdfb7032c306f9c8c554c5a13a
Y0WAIM0 <devstutya.52.2021@doonschool.com>
2024-08-04 17:08:21 +0800

    Updated header and title

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Head from "next/head";
+import {BsFillMoonStarsFill} from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -9,29 +10,31 @@ export default function Home() {
     </Head>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center bg-white/50 backdrop-blur-lg dark:bg-white/20 lg:static lg:w-auto lg:rounded-xl lg:bg-white/50 lg:dark:bg-white/20 p-4">
           &nbsp;
           <code className="text-2xl font-mono font-bold">Devstutya Pandey</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/image.png"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        </p> 
+        <nav>
+        <ul className="flex space-x-6 mb 12">
+          <li>
+            <BsFillMoonStarsFill className="cursor-pointer text 2xl:*: hover:scale-125 transition-transform"/>
+          </li>
+          <li className="hover:scale-125 transition-transform">
+            <a className = "bg-black-500 rounded" href="#resume">Resume</a>
+          </li>
+          <li className="hover:scale-125 transition-transform">
+            <a href="#about">About</a>
+          </li>
+          <li className="hover:scale-125 transition-transform">
+            <a href="#projects">Projects</a>
+          </li>          
+          <li className="hover:scale-125 transition-transform">
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>       
       </div>
+      
 
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] ">
         <Image
