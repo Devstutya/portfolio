19cd7308dbed0ac1512f45d3463ba782bf52226c
Y0WAIM0 <devstutya.52.2021@doonschool.com>
2024-08-11 20:13:11 +0800

    added Projects

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -3,9 +3,9 @@ import { AiOutlineDownload } from "react-icons/ai";
 
 const Navbar = ({ darkMode, toggleDarkMode }) => {
   return (
-    <div className={`fixed left-0 top-0 z-50 flex w-full justify-between items-center font-mono p-3 bg-white/80 backdrop-blur-md rounded-md shadow-md dark:bg-customDarkNavBg dark:border-gray-700`}>
+    <div className={`fixed left-0 top-0 z-50 flex w-full justify-between items-center font-normal p-3 bg-white/80 backdrop-blur-md rounded-md shadow-md dark:bg-customDarkNavBg dark:border-gray-700`}>
       <p className="ml-4">
-        <a href ="#about" className="text-xl font-bold transition-transform hover:scale-110">
+        <a href ="#about" className="text-xl transition-all duration-300 ease-in-out hover:scale-110 hover:text-2xl">
           Portfolio
         </a>
       </p>
@@ -14,29 +14,29 @@ const Navbar = ({ darkMode, toggleDarkMode }) => {
           <li>
             {darkMode ? (
               <BsFillSunFill
-                className="cursor-pointer text-2xl transition-transform hover:scale-110 text-white"
+                className="cursor-pointer text-2xl transition-all duration-300 ease-in-out hover:scale-110 text-white"
                 onClick={toggleDarkMode}
               />
             ) : (
               <BsFillMoonStarsFill
-                className="cursor-pointer text-2xl transition-transform hover:scale-110 text-black"
+                className="cursor-pointer text-2xl transition-all duration-300 ease-in-out hover:scale-110 text-black"
                 onClick={toggleDarkMode}
               />
             )}
           </li>
           <li className="text-xl">
-            <a className="bg-gray-200 dark:bg-gray-800 rounded px-2 py-1 transition-transform hover:scale-110" href="/resume.pdf" download="Devstutya_Pandey_Resume.pdf">
+            <a className="bg-gray-200 dark:bg-gray-800 rounded px-2 py-1 transition-all duration-300 ease-in-out hover:scale-110 hover:text-2xl" href="/resume.pdf" download="Devstutya_Pandey_Resume.pdf">
               Resume <AiOutlineDownload className="inline-block text-xl" />
             </a>
           </li>
           <li className="text-xl">
-            <a className="transition-transform hover:scale-110" href="#skills">Skills</a>
+            <a className="transition-all duration-300 ease-in-out hover:scale-110 hover:text-2xl" href="#skills">Skills</a>
           </li>
           <li className="text-xl">
-            <a className="transition-transform hover:scale-110" href="#projects">Projects</a>
+            <a className="transition-all duration-300 ease-in-out hover:scale-110 hover:text-2xl" href="#projects">Projects</a>
           </li>
           <li className="text-xl">
-            <a className="transition-transform hover:scale-110" href="#contact">Contact</a>
+            <a className="transition-all duration-300 ease-in-out hover:scale-110 hover:text-2xl" href="#contact">Contact</a>
           </li>
         </ul>
       </nav>
--- src/app/page.js
+++ src/app/page.js
@@ -30,9 +30,9 @@ export default function Home() {
         {/* Navbar */}
         <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
         {/*Main content of the page*/}
-        <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="z-10 w-full items-center justify-between text-sm lg:flex">
           <div className="flex flex-col items-center justify-center">
-            <div className="flex items-center justify-center space-x-4 mb-28 ml-12">
+            <div id="about" className="flex font-normal items-center justify-center space-x-4 ml-12">
               <Image
                 src="/FormalPic.jpg"
                 alt="Devstutya Pandey"
@@ -40,24 +40,73 @@ export default function Home() {
                 height={200}
                 className="rounded-full zoomed-right"
               />
-              <div id="about">
-                <h1 className="text-2xl font-mono ml-12">Hi! I&apos;m Devstutya Pandey</h1>
-                <p className="text-xl font-mono mt-8 ml-12">I&apos;m pursuing my Bachelor&apos;s degree at the university of British Columbia, where I am majoring in Computer Science and minoring in Data Science.</p>
-                <p className="text-xl font-mono mt-2 ml-12">This educational journey is sharpening my technical acumen, preparing me to tackle the complex challenges in the ever-growing field of Computer Science.</p>
-                <p className="text-xl font-mono mt-2 ml-12">My goal is to harness this synergy of education and hands-on experience to drive innovation and contribute to groundbreaking projects in my field.</p>
+              <div>
+                <h1 className="text-4xl ml-12">Hi! I&apos;m Devstutya Pandey</h1>
+                <p className="text-2xl font-light mt-8 ml-12">I&apos;m pursuing my Bachelor&apos;s degree at the university of British Columbia, where I am majoring in Computer Science and minoring in Data Science. This educational journey is sharpening my technical acumen, preparing me to tackle the complex challenges in the ever-growing field of Computer Science.</p>
+                <p className="text-2xl font-light mt-2 ml-12">My goal is to harness this synergy of education and hands-on experience to drive innovation and contribute to groundbreaking projects in my field.</p>
               </div>    
             </div>  
             {/* Divider */}
-            <hr className="w-3/4 border-t-2 border-gray-300 my-12" />
+            <hr className="w-3/4 border-t-2 border-gray-300 mt-40 my-12" />
             <div id="skills" className="skills-container">
-              <h2 className="text-5xl font-mono mb-4">Skills</h2>
+              <h2 className="text-5xl font-light mt-20 mb-2">Skills</h2>
               <Logos/>
             </div>  
             {/* Divider */}
-            <hr className="w-3/4 border-t-2 border-gray-300 my-12" />
+            <hr className="w-3/4 border-t-2 border-gray-500 mt-40 my-12" />
             <div id="projects" className="projects-container">
-              <h2 className="text-5xl font-mono mb-4">Projects</h2>
-              <p className="text-xl font-mono">Coming soon...</p>  
+              <h2 className="text-5xl font-light mt-20 mb-12">Projects</h2>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12">
+                <a
+                  href="https://github.com/Devstutya/portfolio"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-card relative p-6 rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 mb-8 max-w-lg mx-auto"
+                >
+                  <h3 className="text-3xl font-medium mb-4">Portfolio Website</h3>
+                  <p className="text-xl text-left font-light mb-4">A modern and responsive portfolio created using react.js, showcasing my skills and projects.</p>
+                  <span className="click-to-view absolute bottom-2 left-2 right-2 text-sm font-medium text-gray-500 opacity-0 transition-opacity duration-300">
+                    Click to view
+                  </span>
+                </a>
+                <a
+                  href="https://github.com/OM200401/Discord-Mods"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-card relative p-6 rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 mb-8 max-w-lg mx-auto"
+                >
+                  <h3 className="text-3xl font-medium mb-4">Discourse</h3>
+                  <p className="text-xl text-left font-light mb-4">An intuitive e-learning platform created using Next.js and Firebase</p>
+                  <span className="click-to-view absolute bottom-2 left-2 right-2 text-sm font-medium text-gray-500 opacity-0 transition-opacity duration-300">
+                    Click to view
+                  </span>
+                </a>
+                <a
+                  href="https://github.com/Jupiter-is-BIG/CS304_FinalProject"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-card relative p-6 rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 mb-8 max-w-lg mx-auto"
+                >
+                  <h3 className="text-3xl font-medium mb-4">Ray&apos;s Groceries</h3>
+                  <p className="text-xl text-left font-light mb-4">A comprehensive online Shopping platform built with PHP and MySQL</p>
+                  <span className="click-to-view absolute bottom-2 left-2 right-2 text-sm font-medium text-gray-500 opacity-0 transition-opacity duration-300">
+                    Click to view
+                  </span>
+                </a>
+                <a
+                  href="https://github.com/Devstutya/ConfDataFetcher"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="project-card relative p-6 rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 mb-8 max-w-lg mx-auto"
+                >
+                  <h3 className="text-3xl font-medium mb-4">Confluence Data Fetcher</h3>
+                  <p className="text-xl text-left font-light mb-4">A python script utilising fastAPI to retrieve data from a specific confluence domain using Confluence&apos;s API</p>
+                  <span className="click-to-view absolute bottom-2 left-2 right-2 text-sm font-medium text-gray-500 opacity-0 transition-opacity duration-300">
+                    Click to view
+                  </span>
+                </a>                
+                {/* Add more projects as needed */}
+              </div>
             </div>    
           </div>
         </div>
